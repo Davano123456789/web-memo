60d0509d94b2abf181d7acfb7f38300ebbeeeb59
DAVANO ALIF RAMADHAN <yustinamalia22@gmail.com>
2026-05-25 15:58:08 +0700

    update services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+            // Force serverless-friendly, database-free drivers on Vercel
+            config([
+                'session.driver' => 'cookie',
+                'cache.default' => 'file',
+                'queue.default' => 'sync',
+                'database.default' => 'sqlite',
+                'database.connections.sqlite.database' => ':memory:',
+            ]);
+        }
     }
 
     /**
